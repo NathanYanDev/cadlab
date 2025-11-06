ce51cc3caedf1c5de7d997fc908d514ed405146f
Nathan Yan <nathan_yan_alves@hotmail.com>
2025-11-06 15:39:09 -0300

    fix: router import

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import { Navbar } from "@/components/navbar";
 import { useAuth } from "@/contexts/AuthContext";
 import { useApiService } from "@/lib/api";
 import { redirect } from "next/dist/server/api-utils";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
   const router = useRouter();
